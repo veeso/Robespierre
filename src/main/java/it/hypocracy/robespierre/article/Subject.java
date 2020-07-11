508a67fe065630d46a0d28e44dd8119b2d2b946a
ChristianVisintin <christian.visintin1997@gmail.com>
2020-07-11 17:23:07 +0200

    Subject

---
+++ src/main/java/it/hypocracy/robespierre/article/Subject.java
@@ -0,0 +1,111 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.article;
+
+import it.hypocracy.robespierre.utils.Uuidv4;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Subject {
+
+  private String id; // UUIDv4
+  protected String name;
+  protected LocalDate birthdate;
+  protected String citizenship; // 2 chars
+  protected String biography;
+  private String remoteId; // Access id for metadata receiver
+  private LocalDateTime lastUpdate; // Last information update
+  public Category category;
+
+  /**
+   * <p>
+   * Instantiates a new Subject
+   * </p>
+   * 
+   * @param id
+   * @param name
+   * @param birthdate
+   * @param citizenship
+   * @param bio
+   * @param remoteId
+   * @param lastUpdate
+   * @param category
+   */
+
+  public Subject(String id, String name, LocalDate birthdate, String citizenship, String bio, String remoteId,
+      LocalDateTime lastUpdate, Category category) {
+    this.id = id;
+    this.name = name;
+    this.birthdate = birthdate;
+    this.citizenship = citizenship;
+    this.biography = bio;
+    this.remoteId = remoteId;
+    this.lastUpdate = lastUpdate;
+    this.category = category;
+  }
+
+  /**
+   * <p>
+   * Instantiates a new subject (doesn't exist in the database)
+   * </p>
+   * 
+   * @param name
+   * @param birthdate
+   * @param citizenship
+   * @param bio
+   * @param remoteId
+   * @param category
+   */
+
+  public Subject(String name, LocalDate birthdate, String citizenship, String bio, String remoteId, Category category) {
+    this.id = new Uuidv4().getUUIDv4();
+    this.name = name;
+    this.birthdate = birthdate;
+    this.citizenship = citizenship;
+    this.biography = bio;
+    this.remoteId = remoteId;
+    this.category = category;
+    // Set last update as now (since subject is new)
+    this.lastUpdate = LocalDateTime.now();
+  }
+
+  // Getters
+
+  public String getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public LocalDate getBirthdate() {
+    return this.birthdate;
+  }
+
+  public String getCitizenship() {
+    return this.citizenship;
+  }
+
+  public String getBiography() {
+    return this.biography;
+  }
+
+  public String getRemoteId() {
+    return this.remoteId;
+  }
+
+  public LocalDateTime getLastUpdate() {
+    return this.lastUpdate;
+  }
+
+}
---
+++ src/test/java/it/hypocracy/robespierre/article/TestSubject.java
@@ -0,0 +1,48 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.article;
+
+import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.Test;
+
+public class TestSubject {
+  /**
+   * Check if a Subject is correctly instantiated
+   */
+  @Test
+  public void shouldInstantiateCategoryNew() {
+    Category cat = new Category("politician");
+    Subject subject = new Subject("foo bar", LocalDate.of(1960, 5, 14), "us", "foo bar is a nice person", "123", cat);
+    assertEquals(subject.getId().length(), 36);
+    assertEquals(subject.getName(), "foo bar");
+    assertEquals(subject.getBiography(), "foo bar is a nice person");
+    assertEquals(subject.getBirthdate().getYear(), 1960);
+    assertEquals(subject.getRemoteId(), "123");
+    assertEquals(subject.getLastUpdate().getHour(), LocalDateTime.now().getHour());
+  }
+
+  @Test
+  public void shouldInstantiateCategory() {
+    String testId = "995b8b98-2103-4873-8457-c2d7436170c5";
+    Category cat = new Category("politician");
+    Subject subject = new Subject(testId, "foo bar", LocalDate.of(1960, 5, 14), "us", "foo bar is a nice person", "123", LocalDateTime.of(2020, 6, 28, 12, 45, 54), cat);
+    assertEquals(subject.getId(), testId);
+    assertEquals(subject.getName(), "foo bar");
+    assertEquals(subject.getBiography(), "foo bar is a nice person");
+    assertEquals(subject.getBirthdate().getYear(), 1960);
+    assertEquals(subject.getRemoteId(), "123");
+    assertEquals(subject.getLastUpdate().getMonth(), Month.JUNE);
+  }
+}
