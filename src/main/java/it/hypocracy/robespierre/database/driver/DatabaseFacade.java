de61e8b75b51f3fa32133800a6a288f2ad5c900e
ChristianVisintin <christian.visintin1997@gmail.com>
2020-07-18 17:56:24 +0200

    Database Facade; MariaDB JDBC

---
+++ src/main/java/it/hypocracy/robespierre/database/driver/DatabaseFacade.java
@@ -0,0 +1,118 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.database.driver;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import it.hypocracy.robespierre.database.query.DeleteQuery;
+import it.hypocracy.robespierre.database.query.InsertQuery;
+import it.hypocracy.robespierre.database.query.SelectQuery;
+import it.hypocracy.robespierre.database.query.UpdateQuery;
+
+public interface DatabaseFacade {
+
+  /**
+   * <p>
+   * Initialize and establish connection with the Database
+   * </p>
+   * 
+   * @throws SQLException
+   */
+
+  public void connect() throws SQLException;
+
+  /**
+   * <p>
+   * Close connection with database
+   * </p>
+   * 
+   * @throws SQLException
+   */
+
+  public void disconnect() throws SQLException;
+
+  /**
+   * <p>
+   * Create a new statement and begins then a new set of actions
+   * </p>
+   * 
+   * @throws SQLException
+   */
+
+  public void startStatement() throws SQLException;
+
+  /**
+   * <p>
+   * Commit changes to database
+   * </p>
+   * 
+   * @throws SQLException
+   */
+
+  public void commit() throws SQLException;
+
+  /**
+   * <p>
+   * Rollback changes to database
+   * </p>
+   * 
+   * @throws SQLException
+   */
+
+  public void rollback() throws SQLException;
+
+  /**
+   * <p>
+   * Perform delete query
+   * </p>
+   * 
+   * @param query
+   * @throws SQLException
+   */
+
+  public void delete(DeleteQuery query) throws SQLException;
+
+  /**
+   * <p>
+   * Perform insert query
+   * </p>
+   * 
+   * @param query
+   * @throws SQLException
+   */
+
+  public void insert(InsertQuery query) throws SQLException;
+
+  /**
+   * <p>
+   * Perform select query
+   * </p>
+   * 
+   * @param query
+   * @return Map
+   * @throws SQLException
+   */
+
+  public Map<String, String> select(SelectQuery query) throws SQLException;
+
+  /**
+   * <p>
+   * Perform update query
+   * </p>
+   * 
+   * @param query
+   * @throws SQLException
+   */
+
+  public void update(UpdateQuery query) throws SQLException;
+
+}
