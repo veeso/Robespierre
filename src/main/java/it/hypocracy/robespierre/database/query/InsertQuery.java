f6520740af0224937bd32fcc05eae961240c062a
ChristianVisintin <christian.visintin1997@gmail.com>
2020-07-18 15:50:40 +0200

    InsertQuery

---
+++ src/main/java/it/hypocracy/robespierre/database/query/InsertQuery.java
@@ -0,0 +1,80 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ *
+ */
+
+package it.hypocracy.robespierre.database.query;
+
+public class InsertQuery implements Query {
+
+  private String table;
+  private String[] columns;
+  private String[] values;
+
+  /**
+   * 
+   * @param table
+   * @param values
+   */
+
+  public InsertQuery(String table, String[] values) {
+    this.table = table;
+    this.values = values;
+    this.columns = null;
+  }
+
+  /**
+   * 
+   * @param table
+   * @param columns
+   * @param values
+   */
+
+  public InsertQuery(String table, String[] columns, String[] values) {
+    this(table, values);
+    this.columns = columns;
+  }
+
+  @Override
+  public String toSQL() {
+    StringBuilder queryStream = new StringBuilder();
+    // Insert into
+    queryStream.append("INSERT INTO ");
+    // Table
+    queryStream.append(table);
+    // If columns is set, set columns
+    if (columns != null) {
+      // Open columns
+      queryStream.append(" (");
+      // Iterate over columns
+      for (String col : columns) {
+        queryStream.append(col);
+        queryStream.append(",");
+      }
+      // Remove last comma
+      queryStream.deleteCharAt(queryStream.length() - 1);
+      // Close columns
+      queryStream.append(")");
+    }
+    // Values
+    queryStream.append(" VALUES (");
+    for (String value : values) {
+      queryStream.append(value);
+      queryStream.append(",");
+    }
+    // Remove last comma
+    queryStream.deleteCharAt(queryStream.length() - 1);
+    // Close values
+    queryStream.append(")");
+    // Terminate query and return
+    queryStream.append(";");
+    return queryStream.toString();
+  }
+
+}
---
+++ src/test/java/it/hypocracy/robespierre/database/query/InsertQueryTest.java
@@ -0,0 +1,42 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.database.query;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InsertQueryTest {
+
+  @Test
+  public void shouldBuildSimpleInsertQuery() {
+    String[] values = new String[4];
+    values[0] = "\"admin\"";
+    values[1] = "\"password1\"";
+    values[2] = "3";
+    values[3] = "True";
+    InsertQuery query = new InsertQuery("user", values);
+    assertEquals(query.toSQL(), "INSERT INTO user VALUES (\"admin\",\"password1\",3,True);");
+  }
+
+  @Test
+  public void shouldBuildDeleteQueryWithColumns() {
+    String[] values = new String[2];
+    values[0] = "\"admin\"";
+    values[1] = "\"password1\"";
+    String[] columns = new String[2];
+    columns[0] = "username";
+    columns[1] = "password";
+    InsertQuery query = new InsertQuery("user", columns, values);
+    assertEquals(query.toSQL(), "INSERT INTO user (username,password) VALUES (\"admin\",\"password1\");");
+  }
+
+}
