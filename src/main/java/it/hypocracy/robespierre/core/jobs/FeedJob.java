b268755708d2bf069140b2081e73cf602dca008b
ChristianVisintin <christian.visintin1997@gmail.com>
2020-08-09 22:11:43 +0200

    Job and FeedJob classes

---
+++ src/main/java/it/hypocracy/robespierre/core/jobs/FeedJob.java
@@ -0,0 +1,59 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.core.jobs;
+
+import java.net.URI;
+
+import it.hypocracy.robespierre.config.FeedSourceConfig;
+import it.hypocracy.robespierre.core.FeedWorker;
+import it.hypocracy.robespierre.core.errors.BusyWorkerException;
+import it.hypocracy.robespierre.feed.FeedSource;
+import it.hypocracy.robespierre.feed.RSSFeedSource;
+import it.hypocracy.robespierre.utils.ISO3166;
+
+public class FeedJob extends Job<FeedWorker> {
+
+  FeedSource source;
+
+  /**
+   * <p>
+   * Instantiatiates a new FeedJob
+   * </p>
+   * 
+   * @param sourceConfig
+   * @throws IllegalArgumentException
+   */
+
+  public FeedJob(FeedSourceConfig sourceConfig) throws IllegalArgumentException {
+    this.interval = sourceConfig.interval;
+    switch (sourceConfig.engine) {
+      case "rss":
+        this.source = new RSSFeedSource(URI.create(sourceConfig.uri), new ISO3166(sourceConfig.country));
+        break;
+    }
+  }
+
+  /**
+   * <p>
+   * Run job on provided feed worker
+   * </p>
+   * 
+   * @throws BusyWorkerException
+   */
+  @Override
+  public void runJob(FeedWorker ptr) throws BusyWorkerException {
+    // Assign job to feed worker
+    ptr.assignNewJob(source);
+    // Re-schedule job
+    this.rescheduleJob();
+  }
+
+}
---
+++ src/main/java/it/hypocracy/robespierre/core/jobs/Job.java
@@ -0,0 +1,55 @@
+/**
+ * @author Christian Visintin <christian.visintin1997@gmail.com>
+ * @version 0.1.0
+ * 
+ * Copyright (C) Christian Visintin - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Christian Visintin <christian.visintin1997@gmail.com>, 2020
+ */
+
+package it.hypocracy.robespierre.core.jobs;
+
+import java.time.LocalDateTime;
+
+public abstract class Job<T> {
+
+  protected int interval; // Seconds
+  // Runtime
+  LocalDateTime nextExecution = LocalDateTime.now();
+
+  /**
+   * <p>
+   * Run Job. Once ran the job is re-scheduled
+   * </p>
+   */
+
+  public abstract void runJob(T ptr) throws Exception;
+
+  /**
+   * <p>
+   * Return next execution time
+   * </p>
+   * 
+   * @return local date time
+   */
+
+  public LocalDateTime getNextExecutionTime() {
+    return nextExecution;
+  }
+
+  /**
+   * <p>
+   * reschedule job execution time
+   * </p>
+   */
+
+  final protected void rescheduleJob() {
+    // Set previous execution
+    LocalDateTime prevExecution = null;
+    prevExecution = LocalDateTime.from(nextExecution);
+    // Sum interval to previous execution
+    nextExecution = prevExecution.plusSeconds(interval);
+  }
+
+}
